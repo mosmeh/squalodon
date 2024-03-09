edf1df2e8d724eae6bf23e0df1e1d09f2237f46f
Yuta Imazu <yuta.imazu@gmail.com>
2024-03-09 19:13:18 +0000

    Respect columns' displayed widths when printing table

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
 use rustyline::error::ReadlineError;
-use squalodon::{Database, Memory, Storage};
+use squalodon::{Database, Memory, Rows};
+use std::io::Write;
+use unicode_width::UnicodeWidthStr;
 
 fn main() -> Result<()> {
     let mut db = Database::new(Memory::new())?;
@@ -22,47 +24,54 @@ fn main() -> Result<()> {
             continue;
         }
         rl.add_history_entry(&buf)?;
-        if let Err(e) = query(&mut db, &buf) {
-            eprintln!("{e}");
-        }
+        let result = db.query(&buf);
         buf.clear();
+        match result {
+            Ok(rows) => write_table(&mut std::io::stdout().lock(), rows)?,
+            Err(e) => eprintln!("{e}"),
+        }
     }
 }
 
-fn query<S: Storage>(db: &mut Database<S>, sql: &str) -> Result<()> {
-    let rows = db.query(sql)?;
+fn write_table<W: Write>(out: &mut W, rows: Rows) -> std::io::Result<()> {
     let columns = rows.columns();
     if columns.is_empty() {
         return Ok(());
     }
     let columns = columns.to_vec();
-    let mut widths: Vec<_> = columns.iter().map(|column| column.name().len()).collect();
+    let mut widths: Vec<_> = columns.iter().map(|column| column.name().width()).collect();
     let mut formatted_rows = Vec::new();
     for row in rows {
         let mut formatted_row = Vec::with_capacity(columns.len());
         for (value, width) in row.columns().iter().zip(widths.iter_mut()) {
             let formatted = value.to_string();
-            *width = (*width).max(formatted.len());
+            *width = (*width).max(formatted.width());
             formatted_row.push(formatted);
         }
         formatted_rows.push(formatted_row);
     }
     for (column, width) in columns.into_iter().zip(&widths) {
-        print!("{:<width$}  ", column.name());
+        write!(out, "{}  ", column.name())?;
+        for _ in column.name().width()..*width {
+            out.write_all(b" ")?;
+        }
     }
-    println!();
+    out.write_all(b"\n")?;
     for width in &widths {
         for _ in 0..*width {
-            print!("-");
+            out.write_all(b"-")?;
         }
-        print!("  ");
+        out.write_all(b"  ")?;
     }
-    println!();
+    out.write_all(b"\n")?;
     for formatted_row in formatted_rows {
         for (formatted, width) in formatted_row.into_iter().zip(&widths) {
-            print!("{formatted:<width$}  ");
+            write!(out, "{formatted}  ")?;
+            for _ in formatted.width()..*width {
+                out.write_all(b" ")?;
+            }
         }
-        println!();
+        out.write_all(b"\n")?;
     }
     Ok(())
 }
